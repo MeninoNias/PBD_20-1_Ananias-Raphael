e67f629898661628fab67b4348c4917dc97e5485
Ananias Raphael <ananias.nobrega@gmail.com>
2021-04-08 17:35:13 -0300

    iniciando o projeto

---
+++ sertao-protocolo/src/main/java/com/pbd/sertaoprotocolo/SertaoProtocoloApplication.java
@@ -0,0 +1,13 @@
+package com.pbd.sertaoprotocolo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SertaoProtocoloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SertaoProtocoloApplication.class, args);
+	}
+
+}
