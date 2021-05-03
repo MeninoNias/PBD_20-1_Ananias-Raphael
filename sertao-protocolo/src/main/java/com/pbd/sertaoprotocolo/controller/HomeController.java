23b9b21ca26d7787c301638d558d600ef78b5c91
Ananias Raphael <ananias.nobrega@gmail.com>
2021-05-03 00:35:30 -0300

    form funcionario

--- sertao-protocolo/src/main/java/com/pbd/sertaoprotocolo/controller/HomeController.java
+++ sertao-protocolo/src/main/java/com/pbd/sertaoprotocolo/controller/HomeController.java
@@ -8,7 +8,7 @@ public class HomeController {
 
     @GetMapping("/")
     public String home(){
-        return "funcionario/listar_funcionario";
+        return "funcionario/form_funcionario";
     }
 
 }
