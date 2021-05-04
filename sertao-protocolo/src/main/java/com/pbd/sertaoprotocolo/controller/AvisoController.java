4895817ebed80d2eefbb653f41a0d173a75a0aad
Ananias Raphael <ananias.nobrega@gmail.com>
2021-05-04 00:32:45 -0300

    aviso controller and template implementation

--- sertao-protocolo/src/main/java/com/pbd/sertaoprotocolo/controller/AvisoController.java
+++ sertao-protocolo/src/main/java/com/pbd/sertaoprotocolo/controller/AvisoController.java
@@ -1,10 +1,21 @@
 package com.pbd.sertaoprotocolo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/avisos")
 public class AvisoController {
+
+    @GetMapping("/listar")
+    public String listAvisos() {
+        return "aviso/listar_aviso";
+    }
+
+    @GetMapping("/new_aviso")
+    public String createAvisos(){
+        return "aviso/form_aviso";
+    }
 
 }
