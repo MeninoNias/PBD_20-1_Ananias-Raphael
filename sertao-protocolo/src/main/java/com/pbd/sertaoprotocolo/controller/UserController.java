db1de593a4445665c85f225403de8519cc9af126
Ananias Raphael <ananias.nobrega@gmail.com>
2021-05-03 23:52:37 -0300

    user controller

--- sertao-protocolo/src/main/java/com/pbd/sertaoprotocolo/controller/UserController.java
+++ sertao-protocolo/src/main/java/com/pbd/sertaoprotocolo/controller/UserController.java
@@ -1,4 +1,16 @@
 package com.pbd.sertaoprotocolo.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/users")
 public class UserController {
+
+    @GetMapping("/listar")
+    public String listUser(){
+        return "user/listar_user";
+    }
+
 }
