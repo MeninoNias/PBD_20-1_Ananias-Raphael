db99df85580f2182824cbf86d52ef99effa63363
Ananias Raphael <ananias.nobrega@gmail.com>
2021-05-03 23:55:50 -0300

    create controllers

---
+++ sertao-protocolo/src/main/java/com/pbd/sertaoprotocolo/controller/AvisoController.java
@@ -0,0 +1,10 @@
+package com.pbd.sertaoprotocolo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/avisos")
+public class AvisoController {
+
+}
---
+++ sertao-protocolo/src/main/java/com/pbd/sertaoprotocolo/controller/SubSetorController.java
@@ -0,0 +1,9 @@
+package com.pbd.sertaoprotocolo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/subsetores")
+public class SubSetorController {
+}
