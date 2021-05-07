c934e3b55495d0eaff24ae41a34e915976fc644f
Ananias Raphael <ananias.nobrega@gmail.com>
2021-05-07 01:16:07 -0300

    login implement

---
+++ sertao-protocolo/src/main/java/com/pbd/sertaoprotocolo/DummyData.java
@@ -0,0 +1,37 @@
+package com.pbd.sertaoprotocolo;
+
+import com.pbd.sertaoprotocolo.model.Role;
+import com.pbd.sertaoprotocolo.model.User;
+import com.pbd.sertaoprotocolo.repository.RoleRepository;
+import com.pbd.sertaoprotocolo.repository.UserRepository;
+import com.pbd.sertaoprotocolo.service.impl.UserServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.util.HashSet;
+import java.util.Set;
+
+@Component
+public class DummyData {
+
+    @Autowired
+    UserServiceImpl userService;
+
+    @Autowired
+    RoleRepository roleRepository;
+
+    @PostConstruct
+    public void saveUser(){
+
+        User user = new User();
+        user.setUserName("admin");
+        user.setActive(true);
+        user.setPassword("admin");
+        user.setEmail("admin@admin.com");
+
+        userService.saveUser(user);
+
+    }
+
+}
