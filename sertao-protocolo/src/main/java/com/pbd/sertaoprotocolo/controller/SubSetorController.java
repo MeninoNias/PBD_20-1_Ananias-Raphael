1bb5fb59a78652cc4e9da663a40bdc40471d6698
Ananias Raphael <ananias.nobrega@gmail.com>
2021-05-16 18:47:14 -0300

    sub setore update implementes

--- sertao-protocolo/src/main/java/com/pbd/sertaoprotocolo/controller/SubSetorController.java
+++ sertao-protocolo/src/main/java/com/pbd/sertaoprotocolo/controller/SubSetorController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -41,6 +42,29 @@ public class SubSetorController {
 
     @RequestMapping(value = "/new_lotacao", method = RequestMethod.POST)
     public ModelAndView registerSubSetor(@Valid SubSetor subSetor, BindingResult result) {
+        ModelAndView modelAndView = new ModelAndView();
+        subSetor.setNome(subSetor.getNome().toUpperCase());
+        if (result.hasErrors()) {
+            modelAndView.setViewName("lotacao/lotacao_form");
+        }
+        lotacaoService.createSubSetor(subSetor);
+        modelAndView.addObject("successMessage", "SubSetor registrado com sucesso");
+        modelAndView.setViewName("redirect:/subsetores/listar");
+        return modelAndView;
+    }
+
+    @GetMapping("/update/{id}")
+    public ModelAndView createSubSetor(@PathVariable("id") Long id) {
+        ModelAndView view = new ModelAndView();
+        SubSetor subSetor = lotacaoService.getSubSetor(id);
+        view.addObject("subSetor", subSetor);
+        view.addObject("setores", setorService.getSetors());
+        view.setViewName("lotacao/lotacao_form");
+        return view;
+    }
+
+    @RequestMapping(value = "/update", method = RequestMethod.POST)
+    public ModelAndView updateSubSetor(@Valid SubSetor subSetor, BindingResult result) {
         ModelAndView modelAndView = new ModelAndView();
         subSetor.setNome(subSetor.getNome().toUpperCase());
         if (result.hasErrors()) {
--- sertao-protocolo/src/main/java/com/pbd/sertaoprotocolo/model/SubSetor.java
+++ sertao-protocolo/src/main/java/com/pbd/sertaoprotocolo/model/SubSetor.java
@@ -1,14 +1,12 @@
 package com.pbd.sertaoprotocolo.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import javax.persistence.*;
 import java.util.List;
 
-@Data
+@Getter
+@Setter
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
